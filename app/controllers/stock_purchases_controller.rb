e98720209d073b8c237db5bf13f5a033680b8cac
francomendoza <frm210@gmail.com>
2015-06-28 22:14:44 -0400

    add devise user

--- app/controllers/stock_purchases_controller.rb
+++ app/controllers/stock_purchases_controller.rb
@@ -6,7 +6,7 @@ class StockPurchasesController < ApplicationController
 
   def create
     # binding.pry
-    @stock_purchase = StockPurchase.new(stock_id: stock_purchase_params[:stock_id], user_id: stock_purchase_params[:user_id], price: stock_purchase_params[:price], original_amount: stock_purchase_params[:original_amount], current_amount: stock_purchase_params[:original_amount], date: stock_purchase_params[:date])
+    @stock_purchase = StockPurchase.new(stock_id: stock_purchase_params[:stock_id], user_id: current_user.id, price: stock_purchase_params[:price], original_amount: stock_purchase_params[:original_amount], current_amount: stock_purchase_params[:original_amount], date: stock_purchase_params[:date])
     if @stock_purchase.save
       redirect_to stock_purchases_path
     else
@@ -26,6 +26,6 @@ class StockPurchasesController < ApplicationController
 
   private
   def stock_purchase_params
-    params.require(:stock_purchase).permit(:stock_id,:user_id,:price,:original_amount,:date)
+    params.require(:stock_purchase).permit(:stock_id,:price,:original_amount,:date)
   end
 end
